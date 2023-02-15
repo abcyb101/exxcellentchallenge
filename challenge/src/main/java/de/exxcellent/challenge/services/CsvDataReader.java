3fd91f2e9df67fbeacb2d50c913552c6330c040b
abcyb101 <abcyb101@outlook.com>
2023-02-15 21:24:31 +0100

    Implemented CsvDataReader and the DataReader class which it extends.

--- challenge/src/main/java/de/exxcellent/challenge/services/CsvDataReader.java
+++ challenge/src/main/java/de/exxcellent/challenge/services/CsvDataReader.java
@@ -1,5 +1,36 @@
 package de.exxcellent.challenge.services;
 
-public class CsvDataReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvDataReader implements DataReader {
+	
+	private String fileName;
+	private String separator;
+	
+	public CsvDataReader(String fileName, String separator) {
+		super();
+		this.fileName = fileName;
+		this.separator = separator;
+	}
+
+
+
+	@Override
+	public List<String[]> readData(String filePath, String format) throws IOException {
+		List<String[]> rows = new ArrayList<>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] values = line.split(separator);
+				rows.add(values);
+			}
+		}
+		
+		return rows;
+	}
 
 }
--- challenge/src/main/java/de/exxcellent/challenge/services/DataReader.java
+++ challenge/src/main/java/de/exxcellent/challenge/services/DataReader.java
@@ -1,5 +1,10 @@
 package de.exxcellent.challenge.services;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface DataReader {
+	
+	List<String[]> readData(String filePath, String format) throws IOException;
 
 }
