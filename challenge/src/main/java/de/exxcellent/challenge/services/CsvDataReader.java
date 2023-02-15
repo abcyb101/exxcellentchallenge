d719588692f00c34e5c7dd25398891f3e1da2812
abcyb101 <abcyb101@outlook.com>
2023-02-16 00:11:18 +0100

    Refactored CsvReader: added logger and error handling.

--- challenge/src/main/java/de/exxcellent/challenge/services/CsvDataReader.java
+++ challenge/src/main/java/de/exxcellent/challenge/services/CsvDataReader.java
@@ -5,10 +5,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import de.exxcellent.challenge.model.Data;
 
 public class CsvDataReader implements DataReader {
+	
+	private static final Logger LOGGER = Logger.getLogger(CsvDataReader.class.getName());
 
 	public CsvDataReader() {
 		super();
@@ -23,7 +27,10 @@ public class CsvDataReader implements DataReader {
 			while ((line = reader.readLine()) != null) {
 				rowList.add(line);
 			}
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Error reading data from CSV file: " + e.getMessage());
 		}
+		LOGGER.log(Level.INFO, "Successfully read data from CSV file.");
 		return rowList;
 	}
 
